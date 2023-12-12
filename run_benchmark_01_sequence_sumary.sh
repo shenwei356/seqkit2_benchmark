da057f8fb0c27ce00d35ebec2a7d3364f3c9bca1
Wei Shen <shenwei356@gmail.com>
2023-12-12 11:09:49 +0000

    add other files

---
+++ run_benchmark_01_sequence_sumary.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+echo Test: Sequence summary
+
+echo -en "\n============================================\n";
+
+echo Plain Text
+
+for f in *.fast{a,q}; do
+
+    echo read file once with cat
+    cat $f > /dev/null;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_old
+    echo data: $f;
+
+    memusg -t -H seqkit_old stat $f > $f.seqkit_old.txt ;
+
+    /bin/rm $f.seqkit_old.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit
+    echo data: $f;
+
+    memusg -t -H seqkit stats $f > $f.seqkit.txt ;
+
+    /bin/rm $f.seqkit.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_j1
+    echo data: $f;
+
+    memusg -t -H seqkit stats $f -j 1 > $f.seqkit.txt ;
+
+    /bin/rm $f.seqkit.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqtk
+    echo data: $f;
+
+    memusg -t -H seqtk size $f > $f.seqtk.txt ;
+
+    /bin/rm $f.seqtk.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqfu
+    echo data: $f;
+
+    memusg -t -H seqfu count $f > $f.seqfu.txt ;
+
+    /bin/rm $f.seqfu.txt;
+done
+
+echo Gzip files
+
+for f in *.fast{a,q}.gz; do
+
+    echo read file once with cat
+    cat $f > /dev/null;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_old
+    echo data: $f;
+
+    memusg -t -H seqkit_old stat $f > $f.seqkit_old.txt ;
+
+    /bin/rm $f.seqkit_old.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit
+    echo data: $f;
+
+    memusg -t -H seqkit stats $f > $f.seqkit.txt ;
+
+    /bin/rm $f.seqkit.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_j1
+    echo data: $f;
+
+    memusg -t -H seqkit stats -j 1 $f > $f.seqkit.txt ;
+
+    /bin/rm $f.seqkit.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqtk
+    echo data: $f;
+
+    memusg -t -H seqtk size $f > $f.seqtk.txt ;
+
+    /bin/rm $f.seqtk.txt;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqfu
+    echo data: $f;
+
+    memusg -t -H seqfu count $f > $f.seqfu.txt ;
+
+    /bin/rm $f.seqfu.txt;
+
+done
---
+++ run_benchmark_03_reverse_complement_sequence.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+echo Test: Reverse complement sequence
+
+echo -en "\n============================================\n";
+
+echo Plain Text
+
+for f in *.fast{a,q}; do
+
+    echo read file once with cat
+    cat $f > /dev/null;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_old
+    echo data: $f;
+
+    memusg -t -H seqkit_old seq -r -p $f -w 0 > $f.seqkit_old.fx ;
+
+    /bin/rm $f.seqkit_old.fx;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit
+    echo data: $f;
+
+    memusg -t -H seqkit seq -r -p $f -w 0 > $f.seqkit.fx ;
+
+    /bin/rm $f.seqkit.fx;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_j1
+    echo data: $f;
+
+    memusg -t -H seqkit seq -r -p $f -w 0 -j 1 > $f.seqkit.fx ;
+
+    /bin/rm $f.seqkit.fx;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqtk
+    echo data: $f;
+
+    memusg -t -H seqtk seq -r $f > $f.seqtk.fx ;
+
+    /bin/rm $f.seqtk.fx;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqfu
+    echo data: $f;
+
+    memusg -t -H seqfu rc $f > $f.seqfu.fx ;
+
+    /bin/rm $f.seqfu.fx;
+done
+
+echo Gzip files
+
+for f in *.fast{a,q}.gz; do
+
+    echo read file once with cat
+    cat $f > /dev/null;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_old
+    echo data: $f;
+
+    memusg -t -H seqkit_old seq -r -p $f -w 0 -o $f.seqkit_old.gz ;
+
+    /bin/rm $f.seqkit_old.gz;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit
+    echo data: $f;
+
+    memusg -t -H seqkit seq -r -p $f -w 0 -o $f.seqkit.gz --compress-level 6 ;
+
+    /bin/rm $f.seqkit.gz;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqkit_j1
+    echo data: $f;
+
+    memusg -t -H seqkit seq -r -p $f -w 0 -j 1 -o $f.seqkit.gz --compress-level 6 ;
+
+    /bin/rm $f.seqkit.gz;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqtk+gzip
+    echo data: $f;
+
+    memusg -t -H seqtk seq -r $f | gzip -c > $f.seqtk.gz ;
+
+    /bin/rm $f.seqtk.gz;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqtk+pigz
+    echo data: $f;
+
+    memusg -t -H seqtk seq -r $f | pigz -p 4 -c > $f.seqtk.gz ;
+
+    /bin/rm $f.seqtk.gz;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqfu+gzip
+    echo data: $f;
+
+    memusg -t -H seqfu rc $f | gzip -c > $f.seqfu.gz ;
+
+    /bin/rm $f.seqfu.gz;
+
+
+    echo -en "\n------------------------------------\n";
+
+    echo == seqfu+pigz
+    echo data: $f;
+
+    memusg -t -H seqfu rc $f | pigz -p 4 -c > $f.seqfu.gz ;
+
+    /bin/rm $f.seqfu.gz;
+done
